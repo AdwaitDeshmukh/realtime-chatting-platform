1ade2b122f32e857b2bc50654e03d5d535a73a6c
AdwaitDeshmukh <adwaitdeshmukh1121@gmail.com>
2026-02-21 00:17:18 +0530

    front edn setup with axios

---
+++ frontend/src/store/useAuthStore.js
@@ -0,0 +1,14 @@
+import {create} from 'zustand';
+
+
+
+export const useAuthStore = create((set) => ({
+  authUser: { name: "john", _id: 123, age: 25 }, 
+  isLoggedin:false,
+
+  login:()=>{
+    console.log("login called");
+    set({isLoggedin:true})
+
+  }
+}));
